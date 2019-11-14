f0b03f4c17938d3b23d183053c2e44fdb1846d1a
Mauricio Robayo <rfmajo@gmail.com>
2019-11-14 11:07:29 -0500

    Add reverse function

--- src/functions.js
+++ src/functions.js
@@ -2,4 +2,7 @@ module.exports = {
   capitalize(text) {
     return text[0].toUpperCase() + text.substring(1).toLowerCase();
   },
+  reverse(text) {
+    return [...text].reverse().join('');
+  }
 };
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,8 +1,14 @@
-const { capitalize } = require('./functions');
+const { capitalize, reverse } = require('./functions');
 
 it('should capitalize a string', () => {
   expect(capitalize('hello, world!')).toBe('Hello, world!');
   expect(capitalize('HELLO, WORLD!')).toBe('Hello, world!');
   expect(capitalize('hello, World!')).toBe('Hello, world!');
   expect(capitalize('¡hola, mundo!')).toBe('¡hola, mundo!');
+});
+
+it('should reverse a string', () => {
+  expect(reverse('hello, world!')).toBe('!dlrow ,olleh');
+  expect(reverse('Hello, World!')).toBe('!dlroW ,olleH');
+  expect(reverse('')).toBe('');
 });
